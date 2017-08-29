6df4d0df6400b9df1ed1e482b33abcf875784499
Bradley Hilton <bradleyhilton@gmail.com>
2017-08-29 10:24:46 -0700

    Added Illuminate Section Groups Queries

--- groups/sectiongroups.py
+++ groups/sectiongroups.py
@@ -11,13 +11,121 @@
 
 import util
 import database.mysql.base
-import database.oracle.base
+# import database.oracle.base
+import database.postgres.base
 # from gservice.groups import BatchGroups, BatchMembers
 
 import gservice.groups
 
 START_TERM = 2600
 END_TERM = START_TERM + 10
+
+get_illuminate_sections = """WITH school_map AS (SELECT sch.site_id
+, CASE WHEN sch.site_id = 6097729 THEN 'BAM'
+	WHEN sch.site_id = 131177 THEN 'BHS' 
+    WHEN sch.site_id = 134924 THEN 'BTA'
+    WHEN sch.site_id = 122804 THEN 'Pre'
+    WHEN sch.site_id = 6090195 THEN 'Crag'
+    WHEN sch.site_id = 6090211 THEN 'Emer'
+    WHEN sch.site_id = 6090252 THEN 'Jeff'
+    WHEN sch.site_id = 6105316 THEN 'JM'
+    WHEN sch.site_id = 6090278 THEN 'LeCo'
+    WHEN sch.site_id = 6090294 THEN 'Long'
+    WHEN sch.site_id = 6090286 THEN 'MX'
+    WHEN sch.site_id = 6056857 THEN 'King'
+    WHEN sch.site_id = 6504 THEN 'NPS'
+    WHEN sch.site_id = 6090302 THEN 'Oxfo'
+    WHEN sch.site_id = 6090187 THEN 'Rosa'
+    WHEN sch.site_id = 6090310 THEN 'TO'
+    WHEN sch.site_id = 6090328 THEN 'Wash'
+    WHEN sch.site_id = 6056865 THEN 'Will'
+    WHEN sch.site_id = 9999999 THEN 'DO'
+    WHEN sch.site_id = 999999999 THEN 'CES'
+    WHEN sch.site_id = 10000000 THEN 'PVTMS'
+    WHEN sch.site_id = 20000000 THEN 'Inact'
+    WHEN sch.site_id = 999999 THEN 'Grad'
+    ELSE NULL
+    END SiteShortName
+, sch.site_name
+FROM sites sch)
+
+SELECT st.section_id AS SECTIONID
+
+, sch.site_id AS SCHOOLID
+, LOWER('z'||sm.SiteShortName||'-'
+	||SUBSTR(tr.term_name, 1,2)||'-'
+	||SUBSTR(u.first_name, 1, 1)||'-'
+	||REGEXP_REPLACE(u.last_name, '\W+', '','g'))
+	||CASE WHEN gl.short_name IN ('6','7','8','9','10','11','12') THEN '-'||REGEXP_REPLACE(LOWER(tb.timeblock_name), '\W+', '','g') ELSE '' END
+	||'@berkeley.net' GROUP_EMAIL
+, 'z '||sm.SiteShortName||' '
+	||SUBSTR(tr.term_name, 1,2)||' '
+	||SUBSTR(u.first_name, 1, 1)||'-'
+	||u.last_name
+	||CASE WHEN gl.short_name IN ('6','7','8','9','10','11','12') THEN '-'||REGEXP_REPLACE(LOWER(tb.timeblock_name), '\W+', '', 'g') ELSE '' END Group_Name
+
+, sm.SiteShortName||' '
+  ||u.Last_Name||' '
+  ||CASE WHEN gl.short_name IN ('6','7','8','9','10','11','12') THEN crs.short_name
+    ELSE tr.term_name||' Period '||REGEXP_REPLACE(tb.timeblock_name, '\W+', '','g') END GROUP_DESCRIPTION
+    
+, tr.term_id AS TERMID
+, u.user_id AS GROUP_OWNER
+-- , u.local_user_id
+-- , u.last_name
+-- , u.first_name
+, crs.school_course_id AS COURSE_NUMBER
+, st.section_id AS SECTION_NUMBER
+
+, crs.short_name AS COURSE_NAME
+--, tb.timeblock_name Period
+
+
+FROM  section_teacher_aff st
+	JOIN section_term_aff sta
+    	ON st.section_id = sta.section_id
+    JOIN terms tr
+    	ON tr.term_id = sta.term_id
+    JOIN sessions ses
+    	ON ses.session_id = tr.session_id
+	JOIN sites sch
+    	ON sch.site_id = ses.site_id
+    LEFT OUTER JOIN grade_levels gl
+		ON gl.grade_level_id = sch.start_grade_level_id
+	JOIN section_course_aff sca
+		ON sca.section_id = st.section_id
+	JOIN courses crs
+		ON crs.course_id = sca.course_id
+    JOIN section_timeblock_aff stb
+    	ON stb.section_id = st.section_id
+    JOIN timeblocks tb
+    	ON tb.timeblock_id = stb.timeblock_id
+	JOIN users u
+		ON u.user_id = st.user_id
+    JOIN school_map AS sm
+        ON sch.site_id = sm.site_id
+
+WHERE ses.academic_year = 2018
+AND gl.short_name IN ('TK','K','1','2','3','4','5','6','7','8','9','10','11','12')
+AND sch.exclude_from_state_reporting = '0'
+ORDER BY group_email
+"""
+
+get_illuminate_schedules = """SELECT ssa.ssa_id as ps_id
+, ssa.student_id AS studentid
+, ssa.section_id AS sectionid
+, ses.site_id AS schoolid
+, ssa.entry_date AS dateenrolled
+, CASE WHEN ssa.leave_date IS NULL THEN tr.end_date ELSE ssa.leave_date END AS dateleft
+FROM section_student_aff AS ssa
+JOIN section_term_aff AS sta
+    ON ssa.section_id = sta.section_id
+JOIN terms AS tr
+    ON sta.term_id = tr.term_id
+JOIN sessions AS ses
+    ON tr.session_id = ses.session_id
+-- WHERE current_date BETWEEN tr.start_date AND tr.end_date
+WHERE '2017-08-29' BETWEEN tr.start_date AND tr.end_date"""
 
 
 
@@ -187,30 +295,35 @@ WHERE sp.studentid IS NULL
 
 
 def refresh_section_groups_data():
-	database.mysql.base.backup_mysql()
+	#database.mysql.base.backup_mysql()
 	# Open an Oracle Cursor
-	ocon = database.oracle.base.CursorWrapper()
-	ocurs = ocon.cursor
+	#ocon = database.oracle.base.CursorWrapper()
+	#ocurs = ocon.cursor
+	# Open Postgres Cursor
+	# Postgres SIS Connection
+	pcon = database.postgres.base.CursorWrapper()
+	pc = pcon.cursor
 	# Open a MySQL Cursor
 	mcon = database.mysql.base.CursorWrapper()
 	mcurs = mcon.cursor
 	# refresh the sections data (groups)
 	mcurs.execute('TRUNCATE TABLE sections_py')
-	util.copy_foreign_table(ocurs, get_sections_from_ps, mcurs, 'sections_py')
+	util.copy_foreign_table(pc, get_illuminate_sections, mcurs, 'sections_py')
 
 	mcurs.execute('TRUNCATE TABLE studentschedule_py')
-	util.copy_foreign_table(ocurs, get_cc_schedule_from_ps, mcurs, 'studentschedule_py')
+	util.copy_foreign_table(ps, get_illuminate_schedules, mcurs, 'studentschedule_py')
 
 	mcon.close()
-	ocon.close()
+	pc.close()
+	#ocon.close()
 
 
 def main():
 	refresh_section_groups_data()
-	gservice.groups.insert_new_groups(new_section_groups)
-	gservice.groups.insert_new_group_members(new_group_owners)
-	gservice.groups.delete_group_members(stale_group_owners)
-	gservice.groups.insert_new_group_members(new_student_group_members)
+	#gservice.groups.insert_new_groups(new_section_groups)
+	#gservice.groups.insert_new_group_members(new_group_owners)
+	#gservice.groups.delete_group_members(stale_group_owners)
+	#gservice.groups.insert_new_group_members(new_student_group_members)
 	
 
 if __name__ == '__main__':
